ce0fcee434676e1e67f961db739ede62242394bd
Vincent Willcox <talking@talktech.info>
2023-04-15 12:58:09 +0100

    Update setup and requirements

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Network Testing appliance
-# uses KIVY, Speedtest-cli
+# uses KIVY, Speedtest.NET
 
 # Installer Script (run as root/sudo please)
 
@@ -15,19 +15,15 @@ fi
 echo "Welcome to Netapp-Touch Installer"
 pause 'Press [Enter] to install the required bits and pieces or CTRL+C to stop...'
 
-apt-get update
-apt-get install python3-pip ethtool
+apt update
+apt install ethtool
 if [ "$?" -ne 0 ]; then
   echo "Error while running apt-get (maybe run apt-get update?)";
   exit 1;
 fi
 echo "Done"
 echo "Now installing python modules"
-pip3 install -U Cython==0.29.9
 pip3 install -r requirements.txt
-pip3 install ipgetter2
-garden install qrcode
-
 
 echo "All done! Enjoy"
 echo "You just need to run python3 main.py"
